82cfc54c3abf36a17d62f63f23d2125500c3c5ae
Abhijeet Muneshwar <openingknots@gmail.com>
2017-05-17 10:33:53 +0530

    GET and POST on topic using topicRepository

--- src/main/java/io/javabrains/springbootquickstart/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootquickstart/topic/TopicService.java
@@ -33,7 +33,7 @@ public class TopicService {
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findOne(id);
     }
 
     public void addTopic(Topic topic) {
